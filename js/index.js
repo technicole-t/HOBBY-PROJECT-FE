230b52f80595f68d98159d9b71b2e75a8938dc32
nicolethorley <thorleynicole@gmail.com>
2020-07-14 14:54:25 +0100

    added axios get for reading trips and trip function for output

--- js/index.js
+++ js/index.js
@@ -9,3 +9,32 @@ function postTrip() {
     .then((res) => res.data)
     .catch((err) => console.log(err));
 }
+
+document.getElementById("readAllTrips").addEventListener("click", function () {
+  axios
+    .get(BASE_URL + "/getTrips")
+    .then((res) => {
+      tripOutput.innerHTML = "";
+      res.data.forEach((el) => {
+        console.log(el);
+        const newTrip = trip(
+          "div",
+          `${el.name}`,
+          tripOutput,
+          "card text-white bg-primary mb-3"
+        );
+        newTrip.addEventListener("click", function () {
+          window.location = "./plan.html?trip=" + el.id;
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+function trip(elementType, text, appendTo, className) {
+  const element = document.createElement(elementType);
+  element.innerText = text;
+  appendTo.appendChild(element);
+  element.className = className;
+  return element;
+}
