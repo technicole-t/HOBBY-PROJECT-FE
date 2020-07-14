1dd1fcabfd52a3f93d9cb5b048b6c726a01e679e
nicolethorley <thorleynicole@gmail.com>
2020-07-14 15:04:15 +0100

    edit and delete journey requests complete

--- js/update.js
+++ js/update.js
@@ -12,3 +12,37 @@ function seeJourneyValues() {
     document.getElementById("update-date-of-travel").value = res.data.date;
   });
 }
+
+function updateAJourney() {
+  const journeyId = new URLSearchParams(window.location.search).get("journey");
+  axios
+    .put(BASE_URL + "/updateJourney/" + journeyId, {
+      departureAirport: document.getElementById("update-departure-airport")
+        .value,
+      destinationAirport: document.getElementById("update-destination-airport")
+        .value,
+      modeOfTravel: document.getElementById("update-mode-of-travel").value,
+      date: document.getElementById("update-date-of-travel").value,
+    })
+    .then((res) => res.data)
+    .then(alert("Journey Sucessfully Updated!!"))
+    .catch((err) => console.log(err));
+}
+
+function deleteData(tripID) {
+  const tripId = new URLSearchParams(window.location.search).get("journey");
+  return fetch(BASE_URL + "/deleteAJourney/" + tripId, {
+    method: "delete",
+  })
+    .then((response) => response.json())
+    .then(alert("Journey Sucessfully deleted"))
+    .catch((err) => console.log(err));
+}
+
+function trip(elementType, text, appendTo, className) {
+  const element = document.createElement(elementType);
+  element.innerText = text;
+  appendTo.appendChild(element);
+  element.className = className;
+  return element;
+}
